78ede40089079f7349a64046a78887253c9f52b3
Mateus Sousa <sousa.programador@gmail.com>
2023-04-15 19:23:20 -0300

    feature: create new class CreteCategoryUse case

---
+++ src/Core/UseCase/DTO/Category/CreateCategory/CategoryCreateInputDto.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Core\UseCase\DTO\Category\CreateCategory;
+
+class CategoryCreateInputDto
+{
+  public function __construct(
+    public string $name,
+    public string $description = '',
+    public bool $isActive = true,
+  ) {
+  }
+}
