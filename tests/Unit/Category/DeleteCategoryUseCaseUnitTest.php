30e5f72300fb03d4fe130aec2b63547aea205078
Mateus Sousa <sousa.programador@gmail.com>
2023-04-16 13:32:28 -0300

    add model category and test category model

--- tests/Unit/Category/DeleteCategoryUseCaseUnitTest.php
+++ tests/Unit/Category/DeleteCategoryUseCaseUnitTest.php
@@ -13,51 +13,51 @@ use stdClass;
 
 class DeleteCategoryUseCaseUnitTest extends TestCase
 {
-  public function testDelete()
-  {
-    $uuid = (string) Uuid::uuid4()->toString();
+    public function testDelete()
+    {
+        $uuid = (string) Uuid::uuid4()->toString();
 
-    $this->mockRepo = Mockery::mock(stdClass::class, CategoryRepositoryInterface::class);
-    $this->mockRepo->shouldReceive('delete')->andReturn(true);
+        $this->mockRepo = Mockery::mock(stdClass::class, CategoryRepositoryInterface::class);
+        $this->mockRepo->shouldReceive('delete')->andReturn(true);
 
-    $this->mockInputDto = Mockery::mock(CategoryInputDto::class, [$uuid]);
+        $this->mockInputDto = Mockery::mock(CategoryInputDto::class, [$uuid]);
 
-    $useCase = new DeleteCategoryUseCase($this->mockRepo);
-    $responseUseCase = $useCase->execute($this->mockInputDto);
+        $useCase = new DeleteCategoryUseCase($this->mockRepo);
+        $responseUseCase = $useCase->execute($this->mockInputDto);
 
-    $this->assertInstanceOf(CategoryDeleteOutputDto::class, $responseUseCase);
-    $this->assertTrue($responseUseCase->success);
+        $this->assertInstanceOf(CategoryDeleteOutputDto::class, $responseUseCase);
+        $this->assertTrue($responseUseCase->success);
 
-    /**
-     * Spies
-     */
-    $this->spy = Mockery::spy(stdClass::class, CategoryRepositoryInterface::class);
-    $this->spy->shouldReceive('delete')->andReturn(true);
-    $useCase = new DeleteCategoryUseCase($this->spy);
-    $responseUseCase = $useCase->execute($this->mockInputDto);
-    $this->spy->shouldHaveReceived('delete');
-  }
+        /**
+         * Spies
+         */
+        $this->spy = Mockery::spy(stdClass::class, CategoryRepositoryInterface::class);
+        $this->spy->shouldReceive('delete')->andReturn(true);
+        $useCase = new DeleteCategoryUseCase($this->spy);
+        $responseUseCase = $useCase->execute($this->mockInputDto);
+        $this->spy->shouldHaveReceived('delete');
+    }
 
-  public function testDeleteFalse()
-  {
-    $uuid = (string) Uuid::uuid4()->toString();
+    public function testDeleteFalse()
+    {
+        $uuid = (string) Uuid::uuid4()->toString();
 
-    $this->mockRepo = Mockery::mock(stdClass::class, CategoryRepositoryInterface::class);
-    $this->mockRepo->shouldReceive('delete')->andReturn(false);
+        $this->mockRepo = Mockery::mock(stdClass::class, CategoryRepositoryInterface::class);
+        $this->mockRepo->shouldReceive('delete')->andReturn(false);
 
-    $this->mockInputDto = Mockery::mock(CategoryInputDto::class, [$uuid]);
+        $this->mockInputDto = Mockery::mock(CategoryInputDto::class, [$uuid]);
 
-    $useCase = new DeleteCategoryUseCase($this->mockRepo);
-    $responseUseCase = $useCase->execute($this->mockInputDto);
+        $useCase = new DeleteCategoryUseCase($this->mockRepo);
+        $responseUseCase = $useCase->execute($this->mockInputDto);
 
-    $this->assertInstanceOf(CategoryDeleteOutputDto::class, $responseUseCase);
-    $this->assertFalse($responseUseCase->success);
-  }
+        $this->assertInstanceOf(CategoryDeleteOutputDto::class, $responseUseCase);
+        $this->assertFalse($responseUseCase->success);
+    }
 
-  protected function tearDown(): void
-  {
-    Mockery::close();
+    protected function tearDown(): void
+    {
+        Mockery::close();
 
-    parent::tearDown();
-  }
+        parent::tearDown();
+    }
 }
