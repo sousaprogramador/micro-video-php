b0dcf72ecc8dc2650e887e94fcfbf2d2b7c40c13
Mateus Sousa <sousa.programador@gmail.com>
2023-04-16 20:33:05 -0300

    create test createCategory respository integration

---
+++ tests/Feature/Core/UseCase/Category/CreateCategoryUseCaseTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Tests\Feature\Core\UseCase\Category;
+
+use App\Models\Category as Model;
+use App\Repositories\Eloquent\CategoryEloquentRepository;
+use Core\UseCase\Category\CreateCategoryUseCase;
+use Core\UseCase\DTO\Category\CreateCategory\CategoryCreateInputDto;
+use Tests\TestCase;
+
+class CreateCategoryUseCaseTest extends TestCase
+{
+    public function test_create()
+    {
+        $repository = new CategoryEloquentRepository(new Model());
+        $useCase = new CreateCategoryUseCase($repository);
+        $responseUseCase = $useCase->execute(
+            new CategoryCreateInputDto(
+                name: 'Teste',
+            )
+        );
+
+        $this->assertEquals('Teste', $responseUseCase->name);
+        $this->assertNotEmpty($responseUseCase->id);
+
+        $this->assertDatabaseHas('categories', [
+            'id' => $responseUseCase->id,
+        ]);
+    }
+}
