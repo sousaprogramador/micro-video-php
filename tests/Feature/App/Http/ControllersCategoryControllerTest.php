06fe684728e255d9293ad2ad541a16b9ced41761
Mateus Sousa <sousa.programador@gmail.com>
2023-04-18 20:18:04 -0300

    add resource store

--- tests/Feature/App/Http/ControllersCategoryControllerTest.php
+++ tests/Feature/App/Http/ControllersCategoryControllerTest.php
@@ -2,8 +2,6 @@
 
 namespace Tests\Feature\App\Http;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 use App\Models\Category;
 use App\Repositories\Eloquent\CategoryEloquentRepository;
