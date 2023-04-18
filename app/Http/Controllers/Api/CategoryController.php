fc4555b9be2287b05bc90afbcd751689767283de
Mateus Sousa <sousa.programador@gmail.com>
2023-04-18 19:58:20 -0300

    implements test CategoriesList

---
+++ app/Http/Controllers/Api/CategoryController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Http\Resources\CategoryResource;
+use Core\UseCase\Category\ListCategoriesUseCase;
+use Core\UseCase\DTO\Category\ListCategories\ListCategoriesInputDto;
+
+class CategoryController extends Controller
+{
+    public function index(Request $request, ListCategoriesUseCase $useCase)
+    {
+        $response = $useCase->execute(
+            input: new ListCategoriesInputDto(
+                filter: $request->get('filter', ''),
+                order: $request->get('order', 'DESC'),
+                page: (int) $request->get('page', 1),
+                totalPage: (int) $request->get('total_page', 15),
+            )
+        );
+
+        return CategoryResource::collection(collect($response->items))
+            ->additional([
+                'meta' => [
+                    'total' => $response->total,
+                    'current_page' => $response->current_page,
+                    'last_page' => $response->last_page,
+                    'first_page' => $response->first_page,
+                    'per_page' => $response->per_page,
+                    'to' => $response->to,
+                    'from' => $response->from,
+                ],
+            ]);
+    }
+}
