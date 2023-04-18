e63e82ad5e843db331132989ce7c696cdfb13cab
Mateus Sousa <sousa.programador@gmail.com>
2023-04-18 20:11:53 -0300

    implements test CategoriesList

--- tests/Feature/App/Http/ControllersCategoryControllerTest.php
+++ tests/Feature/App/Http/ControllersCategoryControllerTest.php
@@ -5,18 +5,36 @@ namespace Tests\Feature\App\Http;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use App\Models\Category;
+use App\Repositories\Eloquent\CategoryEloquentRepository;
+use App\Http\Controllers\Api\CategoryController;
+use Core\UseCase\Category\ListCategoriesUseCase;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
+use Illuminate\Http\Request;
 
 class ControllersCategoryControllerTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
-    public function test_example()
-    {
-        $response = $this->get('/');
+    protected $repository;
 
-        $response->assertStatus(200);
+    protected $controller;
+
+    protected function setUp(): void
+    {
+        $this->repository = new CategoryEloquentRepository(
+            new Category()
+        );
+        $this->controller = new CategoryController();
+
+        parent::setUp();
+    }
+
+    public function test_index()
+    {
+        $useCase = new ListCategoriesUseCase($this->repository);
+
+        $response = $this->controller->index(new Request(), $useCase);
+
+        $this->assertInstanceOf(AnonymousResourceCollection::class, $response);
+        $this->assertArrayHasKey('meta', $response->additional);
     }
 }
