71c596f5687730312f5505b39239f83be31bcc20
Mateus Sousa <sousa.programador@gmail.com>
2023-04-18 19:36:55 -0300

    add is category resources

---
+++ app/Http/Resources/CategoryResource.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Carbon\Carbon;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class CategoryResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'description' => $this->description,
+            'is_active' => $this->is_active,
+            'created_at' => Carbon::make($this->created_at)->format('Y-m-d H:i:s'),
+        ];
+    }
+}
